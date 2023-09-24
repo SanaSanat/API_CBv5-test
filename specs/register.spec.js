d58d0221ca18dca992f2981c56c14000b85aa135
SanaSanat <sst7212@gmail.com>
2023-09-23 22:17:26 -0700

    add register-negative

--- specs/register.spec.js
+++ specs/register.spec.js
@@ -3,22 +3,75 @@ import { register } from '../helpers/general-helper'
 const chance = require('chance').Chance()
 //import 'dotenv/config'
 
-describe('Create a user with valid credentials', () => {
-  let res
-  const newEmail = 'user_' + Date.now() + '@pirate.com'
+describe('Registration', () => {
+  describe('Create a user with valid credentials', () => {
+    let res
+    const newEmail = 'user_' + Date.now() + '@pirate.com'
 
-  before(async () => {
-    res = await register(
-      chance.first(),
-      chance.last(),
-      newEmail,
-      process.env.PASSWORD
-    )
+    before(async () => {
+      res = await register(
+        chance.first(),
+        chance.last(),
+        newEmail,
+        process.env.PASSWORD
+      )
+    })
+    it('verify response status code', async () => {
+      expect(res.statusCode).to.eq(201)
+    })
+    it('verify response message', async () => {
+      expect(res.body.message).contain('created successfully')
+    })
   })
-  it('verify response status code', async () => {
-    expect(res.statusCode).to.eq(201)
+
+  describe('Create a user with invalid firstName, lastName', () => {
+    let res
+    const newEmail = 'user_' + Date.now() + '@pirate.com'
+
+    before(async () => {
+      res = await register('', '', newEmail, process.env.PASSWORD)
+    })
+
+    it('verify status code', async () => {
+      await expect(res.statusCode).to.eq(404)
+    })
+    it('verify response message', async () => {
+      await expect(res.body.message).contain('not created')
+    })
   })
-  it('verify response message', async () => {
-    expect(res.body.message).contain('created successfully')
+
+  describe('Create a user with existing email', () => {
+    let res
+    before(async () => {
+      res = await register(
+        chance.first(),
+        chance.last(),
+        process.env.EMAIL,
+        process.env.PASSWORD
+      )
+    })
+
+    it('verify status code', async () => {
+      await expect(res.statusCode).to.eq(409)
+    })
+    it('verify response message', async () => {
+      await expect(res.body.message).to.eq('User with this e-mail exists')
+    })
+  })
+
+  describe('Create a user with invalid password', () => {
+    let res
+    const newEmail = 'user_' + Date.now() + '@pirate.com'
+
+    before(async () => {
+      res = await register(chance.first(), chance.last(), newEmail, '')
+    })
+
+    it('verify status code', async () => {
+      await expect(res.statusCode).to.eq(400)
+    })
+    it('verify response message', async () => {
+      await expect(res.body.message).to.eq('Wrong password format')
+    })
   })
 })
